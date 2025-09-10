7918ef5712a6e47502d02a2a9d03d87a3b88ae11
Matthias-Carre <lemattheuxbis@gmail.com>
2025-09-10 11:51:48 +0200

    parsing

--- taxi.py
+++ taxi.py
@@ -2,10 +2,13 @@
 # input= 1st line: R C F N B T nexts lines: a b x y s f
 # output= M R_0,R_i... with M number of rides and R_i ride index
 
+def distance(a,b,x,y):
+    return abs(a-x)+abs(b-y)
+
 #greedy
-def greedy(input):
-    
-    return
+def greedy(input,F):
+
+    return res
 
 if __name__ == '__main__':
     
@@ -17,6 +20,11 @@ if __name__ == '__main__':
     F = int(tab[2]) # number of vehicles
 
     for i in range(N):
-        rides.append(input().split(" "))
+        rides.append([i,input().split(" ")])
+
 
     print(rides)
+    print(greedy(rides,F))
+
+
+#10/09 faire full random mais il faut arriver a mettre tt les coureses sur les viucles
