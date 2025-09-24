72810bee023bc7aad0e4d4774a2ea648b524106c
Matthias-Carre <lemattheuxbis@gmail.com>
2025-09-24 10:04:31 +0200

    script fini

--- scriptCompair.sh
+++ scriptCompair.sh
@@ -4,5 +4,7 @@ for file in ./inputs/*.in; do
     base_name=$(basename "$file" .in)
     echo "Processing $base_name"
     
+    first_letter=${base_name:0:1}
+    ./judgeHashCode2018.out "$file" ./outputs/out_"$first_letter".txt 
 
 done
