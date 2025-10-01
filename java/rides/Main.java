29fab45cf01bf18fb4bc3241b8b2f35128890f85
Matthias-Carre <lemattheuxbis@gmail.com>
2025-10-01 11:55:53 +0200

    edit main

--- java/rides/Main.java
+++ java/rides/Main.java
@@ -7,15 +7,17 @@ import java.io.*;
 
 
 public class Main {
-    
-    public static void main(String[] args) throws IOException {
 
+    public static void rundm() throws IOException {
+        Instancedm inst = new Instancedm("./inputs/a_example.in");
+        inst.earlyStartGoal();
+    }
+    public static void runmc() throws IOException {
         Instancemc inst = new Instancemc("./inputs/a_example.in");
         inst.longestRides();
-
-        inst.earlyStartGoal();
-        Instancedm instancedmc = new Instancedm("./inputs/c_no_hurry.in ");
-        Instancedm instancedmd = new Instancedm("./inputs/d_metropolis.in ");
-        instancedmc.testGoal();
-        instancedmd.testGoal();
-    }}
+    }
+    public static void main(String[] args) throws IOException {
+        rundm();
+        //runmc();
+    }
+}
