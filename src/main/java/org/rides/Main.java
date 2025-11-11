24b8182a220a0f51a70b0acd9951ff95dbdd07c2
Matthias Carré <lemattheuxbis@gmail.com>
2025-11-11 19:01:19 +0100

    update main

--- src/main/java/org/rides/Main.java
+++ src/main/java/org/rides/Main.java
@@ -26,75 +26,27 @@ public class Main {
         for (File file : listOfFiles) {
             System.out.println(file.getName()) ;
             Instancemc inst = new Instancemc(file.getPath());
-
             inst.setFileOut(file.getName());
+
+            //fonction utiliser
             int res = inst.earlyStartGoalLS();
+
             addResult(outFile,file+" : " + res );
-            System.out.println(inst.isValid());
+
             inst.setFileOut("ESGLS/"+file.getName());
             inst.out();
         }
     }
 
-    public static void rundm() throws IOException {
-        Instancedm inst = new Instancedm("./inputs/a_example.in");
-        inst.earlyStartGoal();
-        Instancedm instc = new Instancedm("./inputs/c_no_hurry.in");
-        instc.earlyStartGoal();
-        Instancedm instd = new Instancedm("./inputs/d_metropolis.in");
-        instd.testGoal();
-    }
-    public static void runmc() throws IOException {
+    public static void run() throws IOException {
 
         runForFolder("./in","./out_2/res.txt");
 
 
 
-        /*
-        Instancemc inst = new Instancemc("./inputs/a_example.in");
-
-        inst.fileOut = "outmc_a_example.in";
-        inst.longestRides();
-
-        inst = new Instancemc("./inputs/b_should_be_easy.in");
-        inst.longestRides();
-
-        inst = new Instancemc("./inputs/c_no_hurry.in");
-        inst.longestRides();
-
-        inst = new Instancemc("./inputs/d_metropolis.in");
-        inst.longestRides();
-
-        inst = new Instancemc("./inputs/e_high_bonus.in");
-        inst.longestRides();
-
-        inst = new Instancemc("./inputs/group_B_instance.in");
-        inst.longestRides();
-
-        inst = new Instancemc("./inputs/group_C_instance.in");
-        inst.longestRides();
-
-        inst = new Instancemc("./inputs/group_D_instance.in");
-        inst.fileOut ="D";
-        inst.longestRidesLocalSearch();
-        inst.earlyStartGoal();
-
-
-        inst = new Instancemc("./inputs/group-p.in");
-        inst.fileOut ="P";
-        inst.longestRidesLocalSearch();
-        inst.earlyStartGoal();
-
-
-        inst = new Instancemc("./inputs/group-p_big.in");
-        inst.fileOut = "pbig";
-        inst.longestRidesLocalSearch();
-        inst.earlyStartGoal();
-*/
-
     }
     public static void main(String[] args) throws IOException {
-        //rundm();
-        runmc();
+
+        run();
     }
 }
